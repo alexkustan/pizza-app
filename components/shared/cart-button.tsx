5416b35e403bddaf88535b697506bdc877c98a9e
alexkustan <alexkustan@gmail.com>
2024-09-08 20:47:48 +0200

    started on cart

--- components/shared/cart-button.tsx
+++ components/shared/cart-button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '../ui';
 import { ArrowRight, ShoppingCart } from 'lucide-react';
+import { CartDrawer } from './cart-drawer';
 
 interface Props {
     className?: string;
@@ -8,6 +9,7 @@ interface Props {
 
 export const CartButton: React.FC<Props> = ({ className }) => {
   return (
+    <CartDrawer>
     <Button className='group relative'>
     <b>9.99 $</b>
     <span className='h-full w-[1px] bg-white/30 mx-3'/>
@@ -17,5 +19,6 @@ export const CartButton: React.FC<Props> = ({ className }) => {
     </div>
     <ArrowRight className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0' />
  </Button>
+ </CartDrawer>
   );
 };
--- components/shared/cart-drawer.tsx
+++ components/shared/cart-drawer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 
 import {
@@ -9,17 +10,53 @@ import {
     SheetTitle,
     SheetTrigger,
 } from '@/components/ui/sheet'
+import { Button } from '../ui';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
+import { CartDrawerItem } from './cart-drawer-item';
+import { getCartItemDetails } from '@/lib/get-cart-item-details';
 
 interface Props {
     className?: string;
 }
 
 export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,className }) => {
+    const [redirecting, setRedirecting] = React.useState(false)
   return (
     <Sheet>
         <SheetTrigger asChild>{children}</SheetTrigger>
         <SheetContent className='flex flex-col justify-between pb-0 bg-[#F4F1EE]'>
-            
+        <SheetHeader>
+            <SheetTitle>
+                In cart <span className='font-bold'>3 items</span>
+            </SheetTitle>
+        </SheetHeader>
+        <div className='-mx-6 mt-5 overflow-auto flex-1 scrollbar'>
+            <div className='mb-2'>
+                <CartDrawerItem id={1} imageUrl={''} details={getCartItemDetails(2,30,[{name: 'masdkkd'}, {name: 'asdasd'}])} name={'peperoni'} price={8} quantity={1}/>
+            </div> 
+       </div>
+        <SheetFooter className='-mx-6 bg-white p-8 flex-col'>
+            <div className='w-full'>
+                <div className='flex mb-4'>
+                    <span className='flex flex-1 text-lg text-neutral-500'>
+                        Total
+                        <div className='flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2'/>
+                    </span>
+
+                    <span className='font-bold text-lg'>5 $</span>
+                </div>
+            </div>
+            <Link href="/cart">
+            <Button 
+                onClick={() => setRedirecting(true)}
+               type='submit'
+               className='w-full h-12 text-base'>
+              Proced to order
+              <ArrowRight className='w-5 ml-2'/>
+            </Button>
+            </Link>
+        </SheetFooter>
         </SheetContent>
     </Sheet>
   );
